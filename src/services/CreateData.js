69d78740e98d8395b7e96e274861be661b1819f5
AlexVoulg <112186133+AlexVoulg@users.noreply.github.com>
2022-09-26 17:30:36 +0300

    Co-authored-by: Ilias Kordoulas <kordoulakis@users.noreply.github.com>

--- src/services/CreateData.js
+++ src/services/CreateData.js
@@ -1,17 +1,27 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from '../main.js'
 
+//TESTING FUNCTION
+// export async function addSomething(newTravel) {
+//     try {
+//         const docRef = await addDoc(collection(db, "destinations"), {
+//             country: country.value,
+//             description: decription.value,
+//             places: "Rome, Venice, Bologna",
+//             prices: "2000-3500"
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//         console.error("Error adding document: ", e);
+//     }
+// }
 
-export async function addSomething(newTravel) {
+export async function addTravel(travelInfo) {
     try {
-        const docRef = await addDoc(collection(db, "destinations"), {
-            country: country.value,
-            description: decription.value,
-            places: "Rome, Venice, Bologna",
-            prices: "2000-3500"
-        });
+        const docRef = await addDoc(collection(db, "destinations"), travelInfo)
         console.log("Document written with ID: ", docRef.id);
     } catch (e) {
+        console.error(e.message);
         console.error("Error adding document: ", e);
     }
 }
